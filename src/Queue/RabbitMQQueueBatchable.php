a9f5ce87ae0405d8db2ffb01aca60a713ea86a6c
Uladzislau Okan <pupsick@hikka.su>
2023-05-25 13:15:48 +0300

    Reconnect when publish

--- src/Queue/RabbitMQQueueBatchable.php
+++ src/Queue/RabbitMQQueueBatchable.php
@@ -4,9 +4,42 @@ namespace VladimirYuldashev\LaravelQueueRabbitMQ\Queue;
 
 use PhpAmqpLib\Connection\AbstractConnection;
 use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue as BaseRabbitMQQueue;
+use PhpAmqpLib\Exception\AMQPChannelClosedException;
+use PhpAmqpLib\Exception\AMQPConnectionClosedException;
+use PhpAmqpLib\Channel\AMQPChannel;
 
 class RabbitMQQueueBatchable extends BaseRabbitMQQueue
 {
+    protected function publishBasic($msg, $exchange = '', $destination = '', $mandatory = false, $immediate = false, $ticket = null): void
+    {
+        try {
+            parent::publishBasic($msg, $exchange, $destination, $mandatory, $immediate, $ticket);
+        } catch (AMQPConnectionClosedException|AMQPChannelClosedException) {
+            $this->reconnect();
+            parent::publishBasic($msg, $exchange, $destination, $mandatory, $immediate, $ticket);
+        }
+    }
+
+    protected function publishBatch($jobs, $data = '', $queue = null): void
+    {
+        try {
+            parent::publishBatch($jobs, $data, $queue);
+        } catch (AMQPConnectionClosedException|AMQPChannelClosedException) {
+            $this->reconnect();
+            parent::publishBatch($jobs, $data, $queue);
+        }
+    }
+
+    protected function createChannel(): AMQPChannel
+    {
+        try {
+            return parent::createChannel();
+        } catch (AMQPConnectionClosedException) {
+            $this->reconnect();
+            return parent::createChannel();
+        }
+    }
+
     public function push($job, $data = '', $queue = null)
     {
         $queue = $queue ?: $job->onQueue();

e50763a5a67ecbc127cfd184c012f347af2755c8
Sergey Porotikov <sergy.info@gmail.com>
2026-09-23 15:46:46 +0300

    SWR-25399: floor delay queue x-expires at 5 minutes
    Previously x-expires was always ttl * 2, which for short-lived delayed
    jobs produced delay queues that lived only a few seconds/tens of
    seconds. Frequent create/delete churn of these auto-expiring queues
    contributed to RabbitMQ cluster instability (SO-23861,
    "cluster_not_formed"). x-expires is now max(5min, ttl * 2) so every
    delay queue lives at least 5 minutes regardless of message TTL.

--- src/Queue/RabbitMQQueue.php
+++ src/Queue/RabbitMQQueue.php
@@ -28,6 +28,11 @@ use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob;
 class RabbitMQQueue extends Queue implements QueueContract, RabbitMQQueueContract
 {
     /**
+     * Minimum lifetime (in milliseconds) of an auto-declared delay queue, regardless of the message TTL.
+     */
+    private const MIN_DELAY_QUEUE_EXPIRES_MS = 5 * 60 * 1000;
+
+    /**
      * The RabbitMQ connection instance.
      */
     protected ?AbstractConnection $connection = null;
@@ -634,7 +639,7 @@ class RabbitMQQueue extends Queue implements QueueContract, RabbitMQQueueContrac
             'x-dead-letter-exchange' => $this->getExchange(),
             'x-dead-letter-routing-key' => $this->getRoutingKey($destination),
             'x-message-ttl' => $ttl,
-            'x-expires' => $ttl * 2,
+            'x-expires' => max(self::MIN_DELAY_QUEUE_EXPIRES_MS, $ttl * 2),
         ];
     }
 
